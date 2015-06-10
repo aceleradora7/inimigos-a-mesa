3d98a25e2b0e8909f2ad12f4260a2cddc3c21e5c
Kugra <lucas.s.veloso@hotmail.com>
2015-06-10 15:37:08 -0300

    Classe Alimento atualizada conforme a planilha

--- src/main/java/model/Alimento.java
+++ src/main/java/model/Alimento.java
@@ -5,18 +5,37 @@ package model;
  */
 public class Alimento {
 
-    private String id;
+    private int cod;
     private String nome;
-    private double acucar;
-    private double sodio;
-    private double gordura;
+    private double quantidadeAcucarGramas, valorPercentualDiarioAcucar,
+                quantidadeSodioMiligramas, valorPercentualSodio,
+                quantidadeGorduraGramas, valorPercentualGordura,
+                quantidadeCaloria;
 
-    public String getId() {
-        return id;
+    public Alimento(int cod, String nome,
+                    double quantidadeAcucarGramas, double valorPercentualDiarioAcucar,
+                    double quantidadeSodioMiligramas, double valorPercentualSodio,
+                    double quantidadeGorduraGramas, double valorPercentualGordura,
+                    double quantidadeCaloria) {
+
+
+        this.cod = cod;
+        this.nome = nome;
+        this.quantidadeAcucarGramas = quantidadeAcucarGramas;
+        this.valorPercentualDiarioAcucar = valorPercentualDiarioAcucar;
+        this.quantidadeSodioMiligramas = quantidadeSodioMiligramas;
+        this.valorPercentualSodio = valorPercentualSodio;
+        this.quantidadeGorduraGramas = quantidadeGorduraGramas;
+        this.valorPercentualGordura = valorPercentualGordura;
+        this.quantidadeCaloria = quantidadeCaloria;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
     }
 
     public String getNome() {
@@ -27,27 +46,59 @@ public class Alimento {
         this.nome = nome;
     }
 
-    public double getAcucar() {
-        return acucar;
+    public double getQuantidadeAcucarGramas() {
+        return quantidadeAcucarGramas;
     }
 
-    public void setAcucar(double acucar) {
-        this.acucar = acucar;
+    public void setQuantidadeAcucarGramas(double quantidadeAcucarGramas) {
+        this.quantidadeAcucarGramas = quantidadeAcucarGramas;
     }
 
-    public double getSodio() {
-        return sodio;
+    public double getValorPercentualDiarioAcucar() {
+        return valorPercentualDiarioAcucar;
     }
 
-    public void setSodio(double sodio) {
-        this.sodio = sodio;
+    public void setValorPercentualDiarioAcucar(double valorPercentualDiarioAcucar) {
+        this.valorPercentualDiarioAcucar = valorPercentualDiarioAcucar;
     }
 
-    public double getGordura() {
-        return gordura;
+    public double getQuantidadeSodioMiligramas() {
+        return quantidadeSodioMiligramas;
     }
 
-    public void setGordura(double gordura) {
-        this.gordura = gordura;
+    public void setQuantidadeSodioMiligramas(double quantidadeSodioMiligramas) {
+        this.quantidadeSodioMiligramas = quantidadeSodioMiligramas;
+    }
+
+    public double getValorPercentualSodio() {
+        return valorPercentualSodio;
+    }
+
+    public void setValorPercentualSodio(double valorPercentualSodio) {
+        this.valorPercentualSodio = valorPercentualSodio;
+    }
+
+    public double getQuantidadeGorduraGramas() {
+        return quantidadeGorduraGramas;
+    }
+
+    public void setQuantidadeGorduraGramas(double quantidadeGorduraGramas) {
+        this.quantidadeGorduraGramas = quantidadeGorduraGramas;
+    }
+
+    public double getValorPercentualGordura() {
+        return valorPercentualGordura;
+    }
+
+    public void setValorPercentualGordura(double valorPercentualGordura) {
+        this.valorPercentualGordura = valorPercentualGordura;
+    }
+
+    public double getQuantidadeCaloria() {
+        return quantidadeCaloria;
+    }
+
+    public void setQuantidadeCaloria(double quantidadeCaloria) {
+        this.quantidadeCaloria = quantidadeCaloria;
     }
 }
