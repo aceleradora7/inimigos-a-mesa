900590ff0eb0f307db63dc45e02307112fe279c0
othman853 <anuar.yasser@gmail.com>
2015-06-10 10:34:16 -0300

    Realocando classes, Controllers nao sao mapeados se a Main estiver em outro pacote

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -1,4 +1,4 @@
-package br.com.aceleradora.inimigosamesa.app;
+package controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
