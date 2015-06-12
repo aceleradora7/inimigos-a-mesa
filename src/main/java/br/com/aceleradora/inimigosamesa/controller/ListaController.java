53650eb963bbeda7eb7d63e7be2b06ffe77744c9
othman853 <anuar.yasser@gmail.com>
2015-06-12 14:59:03 -0300

    #00 <Rafael, Yasser> - Removendo comentarios

--- src/main/java/br/com/aceleradora/inimigosamesa/controller/ListaController.java
+++ src/main/java/br/com/aceleradora/inimigosamesa/controller/ListaController.java
@@ -30,19 +30,4 @@ public class ListaController {
 
         return "lista";
     }
-
-
-    /*@RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false ,defaultValue="World") String name, Model model){
-        model.addAttribute("name", name);
-        return "greeting";
-    }
-
-    @RequestMapping("/informacao")
-    public String info(){
-        return "info";
-    }*/
-
-
-
 }
