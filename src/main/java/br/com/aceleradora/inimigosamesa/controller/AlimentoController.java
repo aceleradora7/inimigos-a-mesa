4246a8c7cf6633ee6c20002cb1ee8a48682fff63
othman853 <anuar.yasser@gmail.com>
2015-06-12 15:02:14 -0300

    #00 <Rafael, Yasser> - Criando controller para testar banco de dados

---
+++ src/main/java/br/com/aceleradora/inimigosamesa/controller/AlimentoController.java
@@ -0,0 +1,33 @@
+package br.com.aceleradora.inimigosamesa.controller;
+
+import br.com.aceleradora.inimigosamesa.dao.AlimentoRepository;
+import br.com.aceleradora.inimigosamesa.model.Alimento;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AlimentoController {
+
+    @Autowired
+    AlimentoRepository repositorio;
+
+    @RequestMapping("/testaBanco")
+    @ResponseBody
+    public String testeBanco(@RequestParam(value="nome", required = false, defaultValue = "Banana") String nome){
+        try{
+
+            Alimento alimento = new Alimento();
+            alimento.setnomeAlimento(nome);
+
+            repositorio.save(alimento);
+
+        }catch(Exception ex){
+            return "Erro: " + ex.getMessage();
+        }
+
+      return "OK";
+    }
+}
