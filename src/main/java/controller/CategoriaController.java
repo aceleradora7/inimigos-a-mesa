9ee6ebafffc952a14aa7da06a0d53942a47f1396
Kugra <lucas.s.veloso@hotmail.com>
2015-06-12 10:23:54 -0300

    <#3.3<Johan, Guilherme> - Criando botoes para busca por categoria

---
+++ src/main/java/controller/CategoriaController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by aluno03 on 10/06/15.
+ */
+@Controller
+public class CategoriaController {
+    @RequestMapping("/buscaCategoria")
+    public String buscaCategoria(){
+        return "buscaCategoria";
+    }
+}
