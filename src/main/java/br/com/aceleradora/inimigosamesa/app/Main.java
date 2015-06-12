3d40adfd0e37ff1eede93a97950f5fa0ab39893c
othman853 <anuar.yasser@gmail.com>
2015-06-12 14:48:35 -0300

    #00<Rafael, Yasser> - Adicionando anotações na classe main

--- src/main/java/br/com/aceleradora/inimigosamesa/app/Main.java
+++ src/main/java/br/com/aceleradora/inimigosamesa/app/Main.java
@@ -2,10 +2,14 @@ package br.com.aceleradora.inimigosamesa.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"br.com.aceleradora.inimigosamesa"})
+@EnableJpaRepositories(basePackages = {"br.com.aceleradora.inimigosamesa"})
+@EntityScan(basePackages = {"br.com.aceleradora.inimigosamesa"})
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
