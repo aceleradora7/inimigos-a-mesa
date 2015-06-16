bb90529688b63e5c9954758747f7624b8e60a75a
othman853 <anuar.yasser@gmail.com>
2015-06-16 10:24:30 -0300

    #3.2: <Johan, Yasser> - Trocando rota da listagem.

--- src/main/java/br/com/aceleradora/inimigosamesa/controller/ListaController.java
+++ src/main/java/br/com/aceleradora/inimigosamesa/controller/ListaController.java
@@ -7,42 +7,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.Collections;
 
 
 @Controller
 public class ListaController {
 
-
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String listar(Model model) {
-
-        AlimentoDAO alimentodao = new AlimentoDAO();
-
-        model.addAttribute("lists", alimentodao.alimentos);
-
-        return "lista";
-    }
-
-    @RequestMapping(value = "/", method = RequestMethod.POST)
-    public String listar_POST(Model model, @RequestParam(value = "opcao-ordenar", required = false) String tipoOrdenar) {
+    @RequestMapping(value = "/listar", method = RequestMethod.GET)
+    public String listar(Model model,
+                         @RequestParam(value = "ordem-crescente", required = false) boolean ordenar,
+                         @RequestParam(value = "opcao-ordenar", required = false) String tipoOrdenar) {
 
         AlimentoDAO alimentodao = new AlimentoDAO();
 
-        if (tipoOrdenar != null) {
-            if (tipoOrdenar.equals("cre")) {
-                Collections.sort(alimentodao.alimentos);
-            }
+        if(ordenar){
+            Collections.sort(alimentodao.alimentos);
         }
 
-        if (tipoOrdenar != null) {
-            if (tipoOrdenar.equals("decre")) {
-                Collections.sort(alimentodao.alimentos);
-                Collections.reverse(alimentodao.alimentos);
-            }
-        }
 
         model.addAttribute("lists", alimentodao.alimentos);
 
