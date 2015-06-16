152fc4f2cf7ea7ae9d27dc7f480b1b79f1f235cd
othman853 <anuar.yasser@gmail.com>
2015-06-16 10:25:01 -0300

    #3.2: <Johan, Yasser> - Criando controller para páginas de apresentação.

---
+++ src/main/java/br/com/aceleradora/inimigosamesa/controller/ApresentacaoController.java
@@ -0,0 +1,22 @@
+package br.com.aceleradora.inimigosamesa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ApresentacaoController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "home";
+    }
+
+    public String sobre(){
+        return "sobre";
+    }
+
+    public String tutorial(){
+        return "tutorial";
+    }
+
+}
