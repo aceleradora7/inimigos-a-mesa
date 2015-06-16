9796798774fd95e2a7ada437cdc90632c45091cf
lucassveloso <lucas.s.veloso@hotmail.com>
2015-06-16 14:46:44 -0300

    #1.1: <Abimael, Lucas, Yasser> - Unificando controllers

--- src/main/java/br/com/aceleradora/inimigosamesa/controller/AlimentoController.java
+++ src/main/java/br/com/aceleradora/inimigosamesa/controller/AlimentoController.java
@@ -1,12 +1,17 @@
 package br.com.aceleradora.inimigosamesa.controller;
 
+import br.com.aceleradora.inimigosamesa.dao.AlimentoDAO;
 import br.com.aceleradora.inimigosamesa.dao.AlimentoRepository;
 import br.com.aceleradora.inimigosamesa.model.Alimento;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collections;
+import java.util.List;
 
 @Controller
 public class AlimentoController {
@@ -14,21 +19,51 @@ public class AlimentoController {
     @Autowired
     AlimentoRepository repositorio;
 
-    @RequestMapping("/testaBanco")
-    @ResponseBody
-    public String testeBanco(@RequestParam(value="nome", required = false, defaultValue = "Banana") String nome){
-        try{
+    @Autowired
+    AlimentoDAO alimentoDao;
 
-            Alimento alimento = new Alimento();
-            alimento.setnomeAlimento(nome);
 
-            repositorio.save(alimento);
+    @RequestMapping(value = "/lista", method = RequestMethod.GET)
+    public String listar(
+            @RequestParam(value = "opcao-ordenar", required = false, defaultValue = "cre") String tipoDeOrdenacao,
+            Alimento alimento,
+            Model model) {
 
-        }catch(Exception ex){
-            ex.printStackTrace();
-            return "Erro: " + ex.getMessage() + "<br /> ";
+        if (tipoDeOrdenacao != null) {
+            if (tipoDeOrdenacao.equals("cre")) {
+                Collections.sort(alimentoDao.alimentos);
+            }
         }
 
-      return "OK";
+        if (tipoDeOrdenacao != null) {
+            if (tipoDeOrdenacao.equals("decre")) {
+                Collections.sort(alimentoDao.alimentos);
+                Collections.reverse(alimentoDao.alimentos);
+            }
+        }
+
+        model.addAttribute("alimento", new Alimento());
+        model.addAttribute("alimentos", alimentoDao.alimentos);
+
+        return "lista";
+    }
+
+
+    @RequestMapping(value = "/busca", method = RequestMethod.GET)
+    public String busca(Alimento alimento, Model model) {
+        List<Alimento> resultadoBusca = alimentoDao.find(alimento);
+
+        if (!resultadoBusca.isEmpty()) {
+            model.addAttribute("alimentos", resultadoBusca);
+        } else {
+            model.addAttribute("erro", "Nenhum Alimento encontrado!");
+        }
+
+
+        return "lista";
+    }
+
+    public String buscaPorCategoria() {
+        return "lista";
     }
 }
