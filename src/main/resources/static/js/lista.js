e5bf1d2a91f333e48e5e4ed82fe2b4f1c049ae71
othman853 <anuar.yasser@gmail.com>
2015-06-19 11:21:28 -0300

    #00: <Johan, Raphael, Yasser> - Refatorando eventos de clique

--- src/main/resources/static/js/lista.js
+++ src/main/resources/static/js/lista.js
@@ -7,12 +7,35 @@ function associarEventosMenuCategoria(){
     });
 }
 
+function associarEventosOpcaoDeOrdenacao(){
+    var selectOrdenacao = $("opcao-ordenar");
+
+    selectOrdenacao.on("click", onClickSelectDeOrdenacao);
+}
+
+var onClickSelectOrdenacao =  function onClickSelectDeOrdenacao(){
+    var opcaoSelecionada = $(this).find(":selected");
+
+    var link = "/listar?opcao-ordernar=" + opcaoSelecionada.val();
+
+    console.log(link);
+
+    window.location.replace(link);
+};
 
 var onClickBotaoCategoria = function onClickBotaoCategoria(){
     var categoria = $(this).text();
-    window.location.replace("/buscaCategoria?categoria=" + categoria);
-}
+
+    var link = "/buscaCategoria?categoria=" + categoria;
+
+    window.location.replace(link);
+};
+
+var associarEventos = function associarEventos(){
+    associarEventosMenuCategoria();
+    associarEventosOpcaoDeOrdenacao();
+};
 
 $(document).ready(function(){
-    associarEventosMenuCategoria();
+    associarEventos();
 });
