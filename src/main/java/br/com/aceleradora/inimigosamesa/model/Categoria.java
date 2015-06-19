886c567927e9e875853c26dc34a522c1868ca8a5
othman853 <anuar.yasser@gmail.com>
2015-06-19 11:21:11 -0300

    #00: <Johan, Raphael, Yasser> - Mapeando entidade Categoria

--- src/main/java/br/com/aceleradora/inimigosamesa/model/Categoria.java
+++ src/main/java/br/com/aceleradora/inimigosamesa/model/Categoria.java
@@ -1,31 +1,53 @@
 package br.com.aceleradora.inimigosamesa.model;
 
+import javax.annotation.Generated;
+import javax.persistence.*;
+import java.util.List;
+
+@Entity(name = "categoria")
 public class Categoria {
-    private int codCategoria;
-    private String nomeCategoria;
 
-    public Categoria() {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="codcategoria")
+    private int codigo;
+
+    @Column(name="nomecategoria")
+    private String nome;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<Alimento> alimentos;
+
+    public Categoria(){
+
     }
 
-    public Categoria(int codCategoria, String nomeCategoria) {
-        this.codCategoria = codCategoria;
-        this.nomeCategoria = nomeCategoria;
+    public Categoria(int codigo, String nome) {
+        this.codigo = codigo;
+        this.nome = nome;
     }
 
-    public int getCodCategoria() {
-        return codCategoria;
+    public int getCodigo() {
+        return codigo;
     }
 
-    public void setCodCategoria(int codCategoria) {
-        this.codCategoria = codCategoria;
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
     }
 
-    public String getNomeCategoria() {
-        return nomeCategoria;
+    public String getNome() {
+        return nome;
     }
 
-    public void setNomeCategoria(String nomeCategoria) {
-        this.nomeCategoria = nomeCategoria;
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
+    public List<Alimento> getAlimentos() {
+        return alimentos;
+    }
+
+    public void setAlimentos(List<Alimento> alimentos) {
+        this.alimentos = alimentos;
+    }
 }
