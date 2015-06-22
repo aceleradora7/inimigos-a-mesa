fd8010a8cb822d2d8d19d6614c389147296d241d
Kugra <johan.kovalsikoski@gmail.com>
2015-06-22 15:47:56 -0300

    00 <Johan, Lucas> - Apresentando lista para futuro update

--- src/main/java/br/com/aceleradora/inimigosamesa/controller/CategoriaController.java
+++ src/main/java/br/com/aceleradora/inimigosamesa/controller/CategoriaController.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.swing.*;
+import java.util.List;
 
 @Controller
 public class CategoriaController {
@@ -14,21 +18,43 @@ public class CategoriaController {
     @Autowired
     CategoriaRepository categoriaRepository;
 
+    List<Categoria> listaCategoria;
+    int codigo;
 
     @RequestMapping(value = "/cadastrarCategoria", method = RequestMethod.POST)
     public String cadastrar(Categoria categoria, Model model){
-        if(categoriaRepository.save(categoria) == null) {
+
+        if(codigo != 0) {
+            categoria.setCodigo(codigo);
+        }
+
+        if (categoriaRepository.save(categoria) == null) {
             model.addAttribute("mensagemDeRetorno", "Não foi possivel cadastrar categoria");
         } else {
             model.addAttribute("mensagemDeRetorno", "Cadastro realizado com sucesso");
         }
+
+        listaCategoria =  (List<Categoria>) categoriaRepository.findAll();
+
+        model.addAttribute("categorias",listaCategoria);
+
+
+
         return "categoria";
     }
 
+
     @RequestMapping(value="/cadastrarCategoria", method = RequestMethod.GET)
-    public String categoria(Model model){
+    public String categoria(@RequestParam(value = "codigo", required = false) String codigo, Model model){
+        //this.codigo = Integer.parseInt(codigo);
         model.addAttribute("categoria", new Categoria());
+        listaCategoria =  (List<Categoria>) categoriaRepository.findAll();
+        model.addAttribute("categorias",listaCategoria);
+
+
         return "categoria";
     }
 
 }
+
+
