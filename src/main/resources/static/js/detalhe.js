39fad25db588d014f4bbe84c3f4247c3b7e1ef9e
gwm131196 <gwm131196@hotmail.com>
2015-06-25 12:01:20 -0300

    #2.2<Guilherme, Marcus> - Implementando javaScript no calculo das colheres

---
+++ src/main/resources/static/js/detalhe.js
@@ -0,0 +1,10 @@
+function calculaMedidasVisuais() {
+    alert("Hello! I am an alert box!!");
+    var colher;
+    var acucar = $("#valor-calorias").val();
+    alert (acucar + "1");
+
+        if (acucar === 0){
+            colher = acucar%3.5;
+            }
+}
