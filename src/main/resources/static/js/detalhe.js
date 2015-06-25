04e8d85e51d127a27ccd86e5d2f9d06f3629ae57
msulista <marcus.rodrigues81@gmail.com>
2015-06-25 17:06:39 -0300

    #2.2: <Marcus, Yasser> - Corrigindo bug do javaScript que mostra as colheres na tela de acordo com a quantidade de gordura. Falta implementar para as outras e ajustar na tela.

--- src/main/resources/static/js/detalhe.js
+++ src/main/resources/static/js/detalhe.js
@@ -1,26 +1,60 @@
-function calculaMedidasVisuais(){
-    var colher = 0;
-    var calorias = document.getElementById('valor-caloria').innerHTML;
 
-    if (calorias != 0){
-       colher = calorias / 4;
-       console.log("colheres" + colher);
-       var teste = mostraColher(colher);
+function mostraColherGordura(){
+   var colher = 0;
+   // var calorias = document.getElementById('valor-caloria').innerHTML;
+   var calorias =   $("#valor-caloria").attr("value");
+      console.log(calorias);
+      console.log("teste");
 
-       return teste;
-    }
+   if (calorias != 0){
+       colher = calorias / 3;
+       $("#imagem-colher-gordura").empty()
+       for(i=0; i<= colher; i++){
+            $("#imagem-colher-gordura").append($("#template-colher").find("img").clone())
+       }
+   }
 }
+$(document).ready(function(){
+    mostraColherGordura();
+});
 
-function mostraColher(numeroDeColheres) {
 
-    var img = document.createElement("IMG");
-    img.src = "img/colher.png";
 
-    for (i = 0; i <= numeroDeColheres; i++){
-       var colherinha =  colherinha + '1';
-    }
-    return colherinha;
-}
+//function calculaMedidasVisuaisGordura(){
+//    var colher = 0;
+//   // var calorias = document.getElementById('valor-caloria').innerHTML;
+//    var calorias =   $("#valor-caloria").attr("value");
+//    console.log(calorias);
+//    console.log("teste");
+//
+//    if (calorias != 0){
+//       colher = calorias / 3;
+//       console.log("colheres" + colher);
+//       var teste = mostraColherGordura(colher);
+//
+//       return teste;
+//    }
+//}
+
+
+//$("#imagem-colher-gordura").empty()
+//for(i=0;i<3;i++){
+// $("#imagem-colher-gordura").append($("#template-colher").find("img").clone())
+//}
+
+
+//function mostraColher(numeroDeColheres) {
+//
+//    var img = document.createElement("img");
+//    img.src = "img/colher.png";
+//
+//    for (i = 0; i <= numeroDeColheres; i++){
+//       var colherinha =  colherinha + '1';
+//    }
+//    return colherinha;
+//}
+
+
 
 /*"<div id="imagem-colher" class="colher">
                  <img id="image" src="img/colher.png" class="img-rounded media-object colher-img"/>
