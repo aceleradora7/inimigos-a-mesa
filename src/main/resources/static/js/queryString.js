8cc294ed93c9e28077280ba6d27669148a308ee1
Kugra <johan.kovalsikoski@gmail.com>
2015-06-26 16:19:26 -0300

    #00: <Lucas, Yasser> - Manipulacao de Query Strings refatorada

---
+++ src/main/resources/static/js/queryString.js
@@ -0,0 +1,44 @@
+var params = {};
+
+function getParametros(){
+    var parametros = window.location.search;
+
+    function getParametro(parametro){
+        var nome  = parametro.split("=")[0];
+        var valor = parametro.split("=")[1];
+
+        params[nome] = valor;
+    }
+
+    if(parametros.indexOf("?") === -1){
+        return params;
+    }
+
+    parametros = parametros.replace("?", "");
+
+    if(parametros.indexOf("&") === -1){
+        getParametro(parametros);
+        return params;
+    }
+
+    var listaDeParametro = parametros.split("&");
+
+    listaDeParametro.forEach(function(parametro){
+        getParametro(parametro);
+    });
+
+    return params;
+}
+
+
+function setParametro(nome, valor){
+    params[nome] = valor;
+}
+
+function removerParametro(nome){
+    delete params[nome];
+}
+
+function acessarPaginaComParametros(pagina){
+    window.location.href = "/" + pagina + "?" + $.param(params);
+}
