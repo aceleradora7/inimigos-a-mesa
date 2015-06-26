f73daac467b70c1069ef14a1ff1b1a14c9c8398f
gwm131196 <gwm131196@hotmail.com>
2015-06-26 14:25:03 -0300

    #2.2: <Guilherme, Marcus> - Cofigurando as colheres para mostrarem a quantidade de nutrientes de forma ilustrativa

--- src/main/resources/static/js/detalhe.js
+++ src/main/resources/static/js/detalhe.js
@@ -1,61 +1,63 @@
-
+//  <![CDATA[
 function mostraColherGordura(){
    var colher = 0;
-   // var calorias = document.getElementById('valor-caloria').innerHTML;
-   var calorias =   $("#valor-caloria").attr("value");
-      console.log(calorias);
-      console.log("teste");
+   var gordura =   $("#valor-gordura").attr("value");
 
-   if (calorias != 0){
-       colher = calorias / 3;
-       $("#imagem-colher-gordura").empty()
-       for(i=0; i<= colher; i++){
-            $("#imagem-colher-gordura").append($("#template-colher").find("img").clone())
+   if (gordura != 0){
+        colher = gordura / 2;
+        colher = parseInt(colher);
+       $("#imagem-colher-gordura").empty();
+       for(i=0; i <= colher; i++){
+            $("#imagem-colher-gordura").append($("#template-colher").find("img").clone());
        }
+   }else {
+        $("#imagem-colher-gordura").empty();
+        $("#imagem-colher-gordura").append($("#template-sem-colher").find("p").clone());
    }
 }
+
+function mostraColherAcucar(){
+   var colher = 0;
+   var acucar =   $("#valor-acucar").attr("value");
+
+   if (acucar != 0){
+        colher = acucar / 1.76;
+        colher = parseInt(colher);
+       $("#imagem-colher-acucar").empty();
+       for(i=0; i <= colher; i++){
+            $("#imagem-colher-acucar").append($("#template-colher").find("img").clone());
+       }
+   }else {
+            $("#imagem-colher-acucar").empty();
+            $("#imagem-colher-acucar").append($("#template-sem-colher").find("p").clone());
+       }
+}
+
+function mostraColherSal(){
+   var colher = 0;
+   var sodio =   $("#valor-sodio").attr("value");
+   var sal = ((sodio * 100) / 39) *0.001 ;
+   if (sal != 0){
+        colher = sal / 2.06;
+        colher = parseInt(colher);
+       $("#imagem-colher-sal").empty();
+       for(i=0; i <= colher; i++){
+            $("#imagem-colher-sal").append($("#template-colher").find("img").clone());
+       }
+   }else {
+            $("#imagem-colher-sal").empty();
+            $("#imagem-colher-sal").append($("#template-sem-colher").find("p").clone());
+       }
+}
+
+
 $(document).ready(function(){
+
     mostraColherGordura();
-});
+    mostraColherAcucar();
+    mostraColherSal();
+
+})
 
 
-
-//function calculaMedidasVisuaisGordura(){
-//    var colher = 0;
-//   // var calorias = document.getElementById('valor-caloria').innerHTML;
-//    var calorias =   $("#valor-caloria").attr("value");
-//    console.log(calorias);
-//    console.log("teste");
-//
-//    if (calorias != 0){
-//       colher = calorias / 3;
-//       console.log("colheres" + colher);
-//       var teste = mostraColherGordura(colher);
-//
-//       return teste;
-//    }
-//}
-
-
-//$("#imagem-colher-gordura").empty()
-//for(i=0;i<3;i++){
-// $("#imagem-colher-gordura").append($("#template-colher").find("img").clone())
-//}
-
-
-//function mostraColher(numeroDeColheres) {
-//
-//    var img = document.createElement("img");
-//    img.src = "img/colher.png";
-//
-//    for (i = 0; i <= numeroDeColheres; i++){
-//       var colherinha =  colherinha + '1';
-//    }
-//    return colherinha;
-//}
-
-
-
-/*"<div id="imagem-colher" class="colher">
-                 <img id="image" src="img/colher.png" class="img-rounded media-object colher-img"/>
-             </div>" */
+//]]>
