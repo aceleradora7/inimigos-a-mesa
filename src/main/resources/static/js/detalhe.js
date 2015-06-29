c839a33b11f2fecf5956bf6470351e125ae8ce23
lucassveloso <lucas.s.veloso@hotmail.com>
2015-06-29 13:04:52 -0300

    #2.2 <Lucas,Raphael> - Removendo do javascript a regra de negócio de calculo de colheres

--- src/main/resources/static/js/detalhe.js
+++ src/main/resources/static/js/detalhe.js
@@ -1,11 +1,8 @@
 //  <![CDATA[
 function mostraColherGordura(){
-   var colher = 0;
-   var gordura =   $("#valor-gordura").attr("value");
+   var colher = $("#gordura").attr("value");
+   if (colher != 0){
 
-   if (gordura != 0){
-        colher = gordura / 2;
-        colher = parseInt(colher);
        $("#imagem-colher-gordura").empty();
        for(i=0; i <= colher; i++){
             $("#imagem-colher-gordura").append($("#template-colher").find("img").clone());
@@ -17,12 +14,8 @@ function mostraColherGordura(){
 }
 
 function mostraColherAcucar(){
-   var colher = 0;
-   var acucar =   $("#valor-acucar").attr("value");
-
-   if (acucar != 0){
-        colher = acucar / 1.76;
-        colher = parseInt(colher);
+   var colher = $("#acucar").attr("value");
+   if (colher != 0){
        $("#imagem-colher-acucar").empty();
        for(i=0; i <= colher; i++){
             $("#imagem-colher-acucar").append($("#template-colher").find("img").clone());
@@ -34,12 +27,9 @@ function mostraColherAcucar(){
 }
 
 function mostraColherSal(){
-   var colher = 0;
-   var sodio =   $("#valor-sodio").attr("value");
-   var sal = ((sodio * 100) / 39) *0.001 ;
-   if (sal != 0){
-        colher = sal / 2.06;
-        colher = parseInt(colher);
+   var colher = $("#sal").attr("value");
+   if (colher != 0){
+
        $("#imagem-colher-sal").empty();
        for(i=0; i <= colher; i++){
             $("#imagem-colher-sal").append($("#template-colher").find("img").clone());
