337b85dc324cbfa76c51fd41cdbc2d007d6ced44
amandasv <amandasv.pm@gmail.com>
2015-07-01 15:00:48 -0300

    #7.1: <Amanda, Lucas, Yasser> - Implementando script para manipular botoes de paginacao.

---
+++ src/main/resources/static/js/paginacao.js
@@ -0,0 +1,58 @@
+function getCaminho(){
+    return window.location.pathname.replace("/", "");
+}
+
+function onClickPaginas(){
+     var parametros = getParametros();
+     parametros["pagina"] = $(this).text();
+     acessarPaginaComParametros(getCaminho());
+}
+
+function onClickProximaPagina(){
+     var parametros = getParametros();
+     setParametro("pagina", parseInt(parametros["pagina"])+1);
+
+     acessarPaginaComParametros(getCaminho());
+}
+
+function onClickPaginaAnterior(){
+     var parametros = getParametros();
+     setParametro("pagina", parametros["pagina"]-1);
+
+     acessarPaginaComParametros(getCaminho());
+}
+
+function marcaBotaoPaginaAtual(){
+    var paginas = $(".pagination");
+
+    if(getParametros()["pagina"] === 1){
+        $(".pagination .pagina").first().toggleClass("pagina-atual");
+        $(".pagination .pagina").first().on("click", function(){return false;});
+        return;
+    }
+
+    var botoesPaginas = paginas.find(".pagina").each(function(){
+
+        if($(this).text() === getParametros()["pagina"]){
+            $(this).toggleClass("pagina-atual");
+            $(this).on("click", function(){return false;});
+        }
+    });
+}
+
+
+
+function associarEventosPaginacao(){
+    var paginas = $(".pagination");
+    paginas.on("click", ".pagina", onClickPaginas);
+    paginas.on("click", "#proxima-pagina", onClickProximaPagina);
+    paginas.on("click", "#pagina-anterior", onClickPaginaAnterior);
+};
+
+$(document).ready(function(){
+    if(getParametros()["pagina"] === undefined){
+        setParametro("pagina", 1);
+    }
+    associarEventosPaginacao();
+    marcaBotaoPaginaAtual();
+});
