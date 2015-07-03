ca1738d5f84f5f9d8daa3e92d0fe98ab97d723be
lucassveloso <lucas.s.veloso@hotmail.com>
2015-07-03 14:35:23 -0300

    #3.3 <Lucas, Rafael> - Adicionando método findAll com ordenação na CategoriaRepository

--- src/main/java/br/com/aceleradora/inimigosamesa/dao/CategoriaRepository.java
+++ src/main/java/br/com/aceleradora/inimigosamesa/dao/CategoriaRepository.java
@@ -1,7 +1,12 @@
 package br.com.aceleradora.inimigosamesa.dao;
 
 import br.com.aceleradora.inimigosamesa.model.Categoria;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CategoriaRepository extends CrudRepository<Categoria, Integer>{
+
+    List<Categoria> findAll(Sort sort);
 }
