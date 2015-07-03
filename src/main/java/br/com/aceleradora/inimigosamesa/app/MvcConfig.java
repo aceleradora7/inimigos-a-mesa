8edfd7d17b4c11be77dbd4f9c0e92321f6491c8f
othman853 <anuar.yasser@gmail.com>
2015-07-03 14:29:03 -0300

    #00: <Amanda, Yasser> - Mapeando /lista e /grid para o mesmo metodo no AlimentoController.

---
+++ src/main/java/br/com/aceleradora/inimigosamesa/app/MvcConfig.java
@@ -0,0 +1,16 @@
+package br.com.aceleradora.inimigosamesa.app;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter{
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registro) {
+        registro.addViewController("/grid").setViewName("grid");
+        registro.addViewController("/lista").setViewName("lista");
+    }
+
+}
