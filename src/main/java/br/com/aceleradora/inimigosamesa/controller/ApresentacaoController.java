f6e05496ae32fec1512127b651ecce39bddc5717
Kugra <johan.kovalsikoski@gmail.com>
2015-07-06 14:16:15 -0300

    10.1 <Johan, Gabriel, Guilherme> - Mapeando pagina contato com método POST

--- src/main/java/br/com/aceleradora/inimigosamesa/controller/ApresentacaoController.java
+++ src/main/java/br/com/aceleradora/inimigosamesa/controller/ApresentacaoController.java
@@ -1,7 +1,12 @@
 package br.com.aceleradora.inimigosamesa.controller;
 
+import br.com.aceleradora.inimigosamesa.model.RecaptchaResult;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.ui.Model;
 
 @Controller
 public class ApresentacaoController {
@@ -22,6 +27,26 @@ public class ApresentacaoController {
 
     @RequestMapping("/contato")
     public String contato(){
+        return "contato";
+    }
+
+    @RequestMapping(value = "/contato", method = RequestMethod.POST)
+    public String enviaContato(@RequestParam(value="g-recaptcha-response",required = true) String response, Model model){
+        StringBuilder builder = new StringBuilder();
+        builder.append("https://www.google.com/recaptcha/api/siteverify");
+        builder.append("?secret=");
+        builder.append("6Lf3XAkTAAAAALT5KTgNMygdZg7Bo-lO0p_RcO48");
+        builder.append("&response=");
+        builder.append(response);
+        RestTemplate restTemplate = new RestTemplate();
+        RecaptchaResult resultado = restTemplate.getForObject(builder.toString(), RecaptchaResult.class);
+        if(resultado.success) {
+            //ok
+        }
+        else
+        {
+            model.addAttribute("erroCaptcha", true);
+        }
         return "contato";
     }
 
