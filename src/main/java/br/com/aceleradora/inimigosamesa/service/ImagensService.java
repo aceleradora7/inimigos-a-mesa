9e381021826203d2098cba93b79d4b10be0c366f
msulista <marcus.rodrigues81@gmail.com>
2015-07-08 15:58:03 -0300

    #8.2: <Amanda, Marcus> - adicionando caminho da imagem no banco, o caminho não esta completo ainda

--- src/main/java/br/com/aceleradora/inimigosamesa/service/ImagensService.java
+++ src/main/java/br/com/aceleradora/inimigosamesa/service/ImagensService.java
@@ -17,7 +17,8 @@ public class ImagensService {
 
 @Autowired
     public void uploadImagem(){
-        String file = "//home//aluno06//IdeaProjects//inimigos-a-mesa//src//main//java//br//com//aceleradora//inimigosamesa//model//teste.png";
+
+    String file = "//home//aluno06//IdeaProjects//inimigos-a-mesa//src//main//java//br//com//aceleradora//inimigosamesa//model//teste.png";
         Cloudinary cloudinary = new Cloudinary("cloudinary://157778992886617:6Vk3ZiE8qBH4K2j51agKhmH_DL8@dq5mndrjt");
         try {
             cloudinary.uploader().upload(file, ObjectUtils.emptyMap());
