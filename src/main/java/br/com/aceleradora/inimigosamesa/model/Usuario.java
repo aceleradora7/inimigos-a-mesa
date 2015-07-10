e56daa29ea95ca28a25701ef2d74750f7a7ee121
jutorres <torresjujuliana@gmail.com>
2015-07-10 16:02:40 -0300

    #8.1<Juliana,Marcus>Atualizando página de login.

--- src/main/java/br/com/aceleradora/inimigosamesa/model/Usuario.java
+++ src/main/java/br/com/aceleradora/inimigosamesa/model/Usuario.java
@@ -11,7 +11,7 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.TABLE)
     private int codigo;
-    
+
     private String nome;
     private String email;
     private String senha;
