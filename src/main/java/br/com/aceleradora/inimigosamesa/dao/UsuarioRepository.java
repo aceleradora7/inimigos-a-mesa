81ab29156bfac4ddc6f0e9b1a3ed113961c2e364
lucassveloso <lucas.s.veloso@hotmail.com>
2015-07-13 15:07:34 -0300

    #8.1 <Lucas, Marcus> - Criando classe repositório de usuário

---
+++ src/main/java/br/com/aceleradora/inimigosamesa/dao/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package br.com.aceleradora.inimigosamesa.dao;
+
+import br.com.aceleradora.inimigosamesa.model.Usuario;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+/**
+ * Created by aluno02 on 13/07/15.
+ */
+public interface UsuarioRepository extends CrudRepository<Usuario, Integer> {
+
+    List<Usuario> findAll();
+}
