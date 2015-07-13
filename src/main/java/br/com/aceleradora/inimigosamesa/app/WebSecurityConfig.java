697acf4f5e09db3888229f0c5ba0be7425a2fec9
lucassveloso <lucas.s.veloso@hotmail.com>
2015-07-13 15:07:09 -0300

    #8.1 <Lucas, Marcus> - Criando classe  de configuração do Spring Security

---
+++ src/main/java/br/com/aceleradora/inimigosamesa/app/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package br.com.aceleradora.inimigosamesa.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.web.util.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebMvcSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/cadastroAlimento", "/editarAlimento/**").hasRole("ADMIN")
+                .antMatchers("/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login")
+                .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin@d").password("123456").roles("ADMIN");
+    }
+
+
+
+}
