c09340ef36b0dd2e869a09078e0c998874cf5f4d
lucassveloso <lucas.s.veloso@hotmail.com>
2015-07-15 09:00:26 -0300

    0.0 <Lucas> -  Resolvendo merge

--- src/main/java/br/com/aceleradora/inimigosamesa/app/WebSecurityConfig.java
+++ src/main/java/br/com/aceleradora/inimigosamesa/app/WebSecurityConfig.java
@@ -33,6 +33,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
         auth.inMemoryAuthentication().withUser("admin@admin").password("1234").roles("ADMIN");
     }
 
