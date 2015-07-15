41f3028aeec579511e25ca3b95b808745a53ebaf
amandasv <amandasv.pm@gmail.com>
2015-07-15 16:00:37 -0300

    #8.1<Amanda, Guilher, Yasser> Ajustando validacao formulario login

--- src/main/resources/static/js/validacoes.js
+++ src/main/resources/static/js/validacoes.js
@@ -1,41 +1,60 @@
- //  <![CDATA[
+//  <![CDATA[
 
-        function validarRecuperacao(){
-           if( document.dados.inputEmail.value=="" ||
-                document.dados.inputEmail.value.indexOf('@')==-1 ||
-                document.dados.inputEmail.value.indexOf('\.')==-1 )
-           {
-                document.getElementById('trocaEmail').style.display = "inline";
-                document.dados.inputEmail.focus();
+var padraoEmail = new RegExp(/[a-zA-Z0-9\.\_\-]+[@]+(.)+(\.)+[a-zA-Z0-9]+/);
 
-                return false;
+function validarRecuperacao(){
 
-           }
+    var emailRecuperacao = $("#emailRecuperacao");
 
-                return true;
-        }
+   if( padraoEmail.test(emailRecuperacao.val())){
+        return true;
+   }
 
-         function alterandoDisplay(){
-               document.getElementById('trocaEmail').style.display = "none";
-           }
+    $('#erro-email-recuperacao').empty();
+    $('#erro-email-recuperacao').addClass('alert');
+    $('#erro-email-recuperacao').addClass('alert-danger');
+    $('#erro-email-recuperacao').append($("#msg-erro-email").find("label").clone());
+    emailRecuperacao.focus();
+    return false;
+}
 
+function alterandoDisplay(){
+    var erroEmail = $('#erroEmail');
+//    erroEmail.css({"display" : "none"});
 
-         function validarLogin(){
-                   if( document.dados.inputEmail.value=="" ||
-                        document.dados.inputEmail.value.indexOf('@')==-1 ||
-                        document.dados.inputEmail.value.indexOf('\.')==-1 )
-                   {
-                        document.getElementById('trocaEmail').style.display = "inline";
-                        document.dados.inputEmail.focus();
-
-                        return false;
-
-                   }
-
-                        return true;
-                }
+}
 
 
+function validarLogin(){
+
+    var emailLogin = $('#emailLogin');
 
 
- //]]>
+    if(padraoEmail.test(emailLogin.val())){
+        return true;
+    }
+    $('#erro-email-login').empty();
+    $('#erro-email-login').addClass('alert');
+    $('#erro-email-login').addClass('alert-danger');
+    $('#erro-email-login').append($('#msg-erro-email').find("label").clone());
+
+    $("#espacoErro").addClass('form-group');
+
+    emailLogin.focus();
+
+    return false;
+}
+
+
+
+
+$(document).ready(function(){
+
+     $("#btn-entrar").on("click", function(evento){
+             evento.preventDefault();
+             validarLogin()? $("#validarLogin").submit() : console.log("Erro");
+     });
+
+});
+
+//]]>
