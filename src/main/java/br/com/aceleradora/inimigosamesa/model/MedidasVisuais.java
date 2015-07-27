de1117c5fb35a6b40b4dc8e1135c1ea0977bcfc3
Kugra <mbraun.raphael@gmail.com>
2015-07-27 13:14:02 -0300

    #00 <Johan, Lucas, Raphael> - Refatorando código para ajustar ao CodeReview

--- src/main/java/br/com/aceleradora/inimigosamesa/model/MedidasVisuais.java
+++ src/main/java/br/com/aceleradora/inimigosamesa/model/MedidasVisuais.java
@@ -4,53 +4,73 @@ public class MedidasVisuais {
 
     private Alimento alimento;
 
+    public static final double GRAMAS_SAL_COLHER = 2.06;
+    public static final double GRAMAS_ACUCAR_COLHER = 1.76;
+    public static final double ML_GORDURA_COLHER = 2;
+
     public MedidasVisuais(Alimento alimento) {
         this.alimento = alimento;
     }
 
-    public int getColheresDeSal() {
+    public int calculaColherSal() {
         int colher = 0;
 
-
-        if (alimento.getSodio() == null || alimento.getSodio().equals("")) {
-            return colher;
-        }
-
-        double sodio = Double.parseDouble(alimento.getSodio());
-        double sal = ((sodio * 100) / 39) * 0.001;
-        if (sal != 0) {
-            colher = (int) (sal / 2.06);
+        if (!verificaColherVazia(alimento.getSodio())) {
+            double sodio = Double.parseDouble(alimento.getSodio());
+            double sal = converteSodioEmSal(sodio);
+            if (sal != 0) {
+                colher = (int) (sal / GRAMAS_SAL_COLHER);
+            }
         }
         return colher;
+    }
+
+    public double converteSodioEmSal(double sodio) {
+        return ((sodio * 100) / 39) * 0.001;
+    }
+
+    public int calculaColherAcucar() {
+        int colher = 0;
+
+        if (!verificaColherVazia(alimento.getAcucar())) {
+            double acucar = Double.parseDouble(alimento.getAcucar());
+            if (acucar != 0) {
+                colher = (int) (acucar / GRAMAS_ACUCAR_COLHER);
+            }
+        }
+
+        return colher;
+    }
+
+    public int calculaColherGordura() {
+        int colher = 0;
+
+        if (!verificaColherVazia(alimento.getGordura())) {
+            double gordura = Double.parseDouble(alimento.getGordura());
+            if (gordura != 0) {
+                colher = (int) (gordura / ML_GORDURA_COLHER);
+            }
+        }
+        return colher;
+    }
+
+    public boolean verificaColherVazia(String valor) {
+        if (valor == null || valor.equals("")) {
+            return true;
+        }
+        return false;
+    }
+
+    public int getColheresDeSal() {
+        return calculaColherSal();
     }
 
     public int getColheresDeAcucar() {
-        int colher = 0;
-
-
-        if (alimento.getAcucar() == null || alimento.getAcucar().equals("")) {
-            return colher;
-        }
-
-        double acucar = Double.parseDouble(alimento.getAcucar());
-        if (acucar != 0) {
-            colher = (int) (acucar / 1.76);
-        }
-        return colher;
+        return calculaColherAcucar();
     }
 
     public int getColheresDeGordura() {
-        int colher = 0;
-
-
-        if (alimento.getGordura() == null || alimento.getGordura().equals("")) {
-            return colher;
-        }
-
-        double gordura = Double.parseDouble(alimento.getGordura());
-        if (gordura != 0) {
-            colher = (int) (gordura / 2);
-        }
-        return colher;
+        return calculaColherGordura();
     }
+
 }
