88863a0f95889f5b233e31513d96f7f0ee7587dc
lucassveloso <lucas.s.veloso@hotmail.com>
2015-07-31 14:42:04 -0300

    #0.0 <Lucas, Johan> - Adicionando método de busca por nome no serviço de categorias

--- src/main/java/br/com/aceleradora/inimigosamesa/service/CategoriaService.java
+++ src/main/java/br/com/aceleradora/inimigosamesa/service/CategoriaService.java
@@ -1,16 +1,24 @@
 package br.com.aceleradora.inimigosamesa.service;
 
 import br.com.aceleradora.inimigosamesa.dao.CategoriaRepository;
+import br.com.aceleradora.inimigosamesa.model.Alimento;
 import br.com.aceleradora.inimigosamesa.model.Categoria;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CategoriaService {
 
+
     @Autowired
     private CategoriaRepository categoriaRepository;
+
+    public List<Categoria> buscaPorNome(String nome) {
+        return categoriaRepository.findByNome(nome);
+    }
 
     public Iterable<Categoria> buscaTodos(Sort sort){
         return categoriaRepository.findAll(sort);
