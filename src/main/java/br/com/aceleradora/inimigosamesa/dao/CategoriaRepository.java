880c6c17f9b2ebf37bddc603950d39423c579a24
Kugra <johan.kovalsikoski@gmail.com>
2015-07-31 14:38:32 -0300

    #0.0 <Lucas, Johan> - Adicionando método de busca por nome de categorias no repositorio

--- src/main/java/br/com/aceleradora/inimigosamesa/dao/CategoriaRepository.java
+++ src/main/java/br/com/aceleradora/inimigosamesa/dao/CategoriaRepository.java
@@ -1,12 +1,19 @@
 package br.com.aceleradora.inimigosamesa.dao;
 
 import br.com.aceleradora.inimigosamesa.model.Categoria;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CategoriaRepository extends CrudRepository<Categoria, Integer>{
+
+    @Query(Consultas.Categoria.FIND_BY_NOME)
+    List<Categoria> findByNome(@Param("nome") String nome);
 
     List<Categoria> findAll(Sort sort);
 }
