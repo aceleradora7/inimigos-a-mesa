220a55f9a7dd5b0bc99106bfae45da6c379efb61
thiagogp <tgpellegrin@gmail.com>
2015-08-03 15:08:54 -0300

    #0.0<Thiago, Raphael> Implementando busca por letra na categoria.

--- src/main/java/br/com/aceleradora/inimigosamesa/dao/Consultas.java
+++ src/main/java/br/com/aceleradora/inimigosamesa/dao/Consultas.java
@@ -6,6 +6,7 @@ package br.com.aceleradora.inimigosamesa.dao;
 public class Consultas {
     public class Alimento{
         public static final String FIND_BY_NOME = "SELECT a FROM alimento a WHERE UPPER(UNACCENT(nome)) LIKE UPPER(UNACCENT(:nome))";
+        public static final String FIND_BY_NOME_NA_CATEGORIA = "SELECT a FROM alimento a WHERE cod_categoria = :categoria AND UPPER(UNACCENT(nome)) LIKE UPPER(UNACCENT(:nome))";
     }
     public class Categoria{
         public static final String FIND_BY_NOME = "SELECT a FROM categoria a WHERE UPPER(UNACCENT(nome)) LIKE UPPER(UNACCENT(:nome))";
