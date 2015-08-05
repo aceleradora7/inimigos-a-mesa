e69f8ff02a75185e85f5a09325896eec32dad227
gwm131196 <gwm131196>
2015-08-05 13:13:54 -0300

    #13 <Marcus, Thiago, Patricia> - Criando classe de testes da Calculadora

---
+++ src/test/model/CalculadoraTeste.java
@@ -0,0 +1,18 @@
+package model;
+
+import br.com.aceleradora.inimigosamesa.model.Calculadora;
+import org.junit.Test;
+
+/**
+ * Created by aluno03 on 05/08/15.
+ */
+public class CalculadoraTeste {
+
+    Calculadora calculadora = new Calculadora();
+
+    @Test
+    public void QuandoPassamosUmAlimentoEntaoRetornaOProorioValor() throws Exception {
+
+
+    }
+}
