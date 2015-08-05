6fdf5cdae26ecf431f6d3b94c2ca9fee09478e6a
gwm131196 <gwm131196>
2015-08-05 17:40:43 -0300

    #13 <Eduardo, Marcus> - Refatorando adicionando novos campos na estancia

--- src/test/model/AlimentoTest.java
+++ src/test/model/AlimentoTest.java
@@ -14,7 +14,7 @@ public class AlimentoTest {
 
     @Test
     public void quandoValorForIgualANullEntaoRetornaOMesmoValor() throws Exception {
-        Alimento alimento = new Alimento("abacate", "0", "0", null, "0", "0", "0", "0", "0", "0", "0", "0", new Categoria(1));
+        Alimento alimento = new Alimento("abacate", "0", "0", null, "0", "0", "0", "0", "0", "0", "0", "0", new Categoria(1),"","");
 
         String valorEsperado = null;
         String valorRetornado = alimento.calculaExibicao(alimento.getGordura());
@@ -24,7 +24,7 @@ public class AlimentoTest {
 
     @Test
     public void quandoValorForIgualAVazioEntaoRetornaOMesmoValor() throws Exception {
-        Alimento alimento = new Alimento("abacate", "0", "0", "", "0", "0", "0", "0", "0", "0", "0", "0", new Categoria(1));
+        Alimento alimento = new Alimento("abacate", "0", "0", "", "0", "0", "0", "0", "0", "0", "0", "0", new Categoria(1),"","");
 
         String valorEsperado = "";
         String valorRetornado = alimento.calculaExibicao(alimento.getGordura());
@@ -34,7 +34,7 @@ public class AlimentoTest {
 
     @Test
     public void quandoPorcaoExibicaoForIgualAPorcaoBaseCalculoEntaoRetornaOMesmoValor() throws Exception {
-        Alimento alimento = new Alimento("abacate", "0", "0", "10", "0", "100", "0", "100", "0", "0", "0", "0", new Categoria(1));
+        Alimento alimento = new Alimento("abacate", "0", "0", "10", "0", "100", "0", "100", "0", "0", "0", "0", new Categoria(1),"","");
 
         String valorEsperado = "10";
         String valorRetornado = alimento.calculaExibicao(alimento.getGordura());
@@ -44,7 +44,7 @@ public class AlimentoTest {
 
     @Test
     public void quandoPorcaoExibicaoForMenorQueAPorcaoBaseCalculoEntaoCalculaOValorASerExibido() throws Exception {
-        Alimento alimento = new Alimento("abacate", "0", "0", "10", "0", "100", "0", "70", "0", "0", "0", "0", new Categoria(1));
+        Alimento alimento = new Alimento("abacate", "0", "0", "10", "0", "100", "0", "70", "0", "0", "0", "0", new Categoria(1),"","");
 
         String valorEsperado = "7.0";
         String valorRetornado = alimento.calculaExibicao(alimento.getGordura());
@@ -54,7 +54,7 @@ public class AlimentoTest {
 
     @Test
     public void quandoPorcaoExibicaoForMaiorQueAPorcaoBaseCalculoEntaoCalculaOValorASerExibido() throws Exception {
-        Alimento alimento = new Alimento("abacate", "0", "0", "10", "0", "100", "0", "200", "0", "0", "0", "0", new Categoria(1));
+        Alimento alimento = new Alimento("abacate", "0", "0", "10", "0", "100", "0", "200", "0", "0", "0", "0", new Categoria(1),"","");
 
         String valorEsperado = "20.0";
         String valorRetornado = alimento.calculaExibicao(alimento.getGordura());
@@ -64,14 +64,14 @@ public class AlimentoTest {
 
     @Test
     public void quandoPorcaoBaseDeCalculoForNullEntaoRetornaFalse() throws Exception {
-        Alimento alimento = new Alimento("abacate", "0", "0", "10", "0", null, "0", "200", "0", "0", "0", "0", new Categoria(1));
+        Alimento alimento = new Alimento("abacate", "0", "0", "10", "0", null, "0", "200", "0", "0", "0", "0", new Categoria(1),"","");
 
         assertTrue(!alimento.temLetraPorcaoBaseCalculo());
     }
 
     @Test
     public void quandoPorcaoBaseDeCalculoForVaziaEntaoRetornaFalse() throws Exception {
-        Alimento alimento = new Alimento("abacate", "0", "0", "10", "0", null, "0", "200", "0", "0", "0", "0", new Categoria(1));
+        Alimento alimento = new Alimento("abacate", "0", "0", "10", "0", null, "0", "200", "0", "0", "0", "0", new Categoria(1),"","");
 
         assertTrue(!alimento.temLetraPorcaoBaseCalculo());
     }
