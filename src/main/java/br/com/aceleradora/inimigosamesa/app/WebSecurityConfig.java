720c23f0ce9a49eff6c06cd31e109cd00b1474d4
amandasv <amandasv.pm@gmail.com>
2015-08-06 10:51:17 -0300

    #8.3 <Eduardo, Lucas> Adicionando passwordEnconder

--- src/main/java/br/com/aceleradora/inimigosamesa/app/WebSecurityConfig.java
+++ src/main/java/br/com/aceleradora/inimigosamesa/app/WebSecurityConfig.java
@@ -41,7 +41,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());;
 
     }
 }
