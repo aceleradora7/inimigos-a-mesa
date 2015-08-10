b56eb62049ed881cf25b1af4a0adc6835d3ab944
raphaelbraun <mbraun.raphael@gmail.com>
2015-08-10 14:55:07 -0300

    #14 <Guilherme, Raphael> Adicionando js

--- src/main/resources/static/js/home3.js
+++ src/main/resources/static/js/home3.js
@@ -14,4 +14,33 @@ $(function() {
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+
+    function efeitoDaAncora(){
+        $(function() {
+        $('a').bind('click',function(event){
+        var $anchor = $(this);
+        $('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top}, 750,'swing');
+        });
+        });
+    }
+
+
+    function efeitoDaHome(){
+        $(function() {
+        $('a').bind('click',function(event){
+        var $anchor = $(this);
+        $('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top}, 750,'swing');
+        });
+        });
+    }
+
+
+$(document).ready(function(){
+
+    efeitoDaAncora();
+
+})
+
+
 /*]]>*/
