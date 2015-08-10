ee118aa529519fd2de183d21fc63b8be190267be
lucassveloso <lucas.s.veloso@hotmail.com>
2015-08-10 15:53:07 -0300

    #13 <Lucas, Johan> - Refatorando testes da calculadora

--- src/test/java/br/com/aceleradora/inimigosamesa/model/CalculadoraTeste.java
+++ src/test/java/br/com/aceleradora/inimigosamesa/model/CalculadoraTeste.java
@@ -23,67 +23,47 @@ public class CalculadoraTeste {
         assertEquals("Não deve retornar null:",valorEsperado,valorRetornado);
     }
 
-    @Test
-    public  void adicionaAlimentoTeste(){
-        Calculadora calculadora = new Calculadora();
-        Alimento alimento = new Alimento();
-
-        calculadora.adicionaAlimento(alimento);
-
-        boolean valorEsperado = false;
-        boolean valoRetornado = calculadora.adicionaAlimento(alimento);
-
-        assertEquals("Retorna false se ja existir o alimento: ",valorEsperado,valoRetornado);
-
-        Alimento alimento2 = new Alimento();
-        alimento.setCodigo(3);
-        valorEsperado = true;
-        valoRetornado = calculadora.adicionaAlimento(alimento2);
-
-        assertEquals("Retorna true se o alimento poder ser adicionado: ",valorEsperado,valoRetornado);
-    }
-
-    @Test
-    public void somaTotalDeAlimentosDaCalculadoraTest(){
-        Calculadora calculadora = new Calculadora();
-        
-        Alimento alimento2 = new Alimento();
-
-        alimento2.setCodigo(1);
-
-        double valorEsperado = 0.0;
-
-        double valorRetornado = calculadora.getCaloriasTotal();
-        assertEquals("Deve retornar zero calorias.", valorEsperado, valorRetornado, 0);
-        valorRetornado = calculadora.getGorduraTotal();
-        assertEquals("Deve retornar zero gordura.",valorEsperado,valorRetornado,0);
-        valorRetornado = calculadora.getSodioTotal();
-        assertEquals("Deve retornar zero sal.",valorEsperado,valorRetornado,0);
-        valorRetornado = calculadora.getAcucarTotal();
-        assertEquals("Deve retornar zero açucar.",valorEsperado,valorRetornado,0);
-
-        calculadora.adicionaAlimento(alimento2);
-
-        valorEsperado = 2000.0;
-        valorRetornado = calculadora.getCaloriasTotal();
-        assertEquals("Deve retornar 2000 calorias.",valorEsperado,valorRetornado,0);
-
-        valorEsperado = 2000.0;
-        valorRetornado = calculadora.getGorduraTotal();
-        assertEquals("Deve retornar 2000 gordura.",valorEsperado,valorRetornado,0);
-
-        valorEsperado = 5.0;
-        valorRetornado = calculadora.getSodioTotal();
-        assertEquals("Deve retornar 2000 sodio.",valorEsperado,valorRetornado,0);
-
-        valorEsperado = 2000.0;
-        valorRetornado = calculadora.getAcucarTotal();
-        assertEquals("Deve retornar 2000 acucar.",valorEsperado,valorRetornado,0);
-
-
-        valorEsperado = 0.01;
-        valorRetornado = calculadora.getSalTotal();
-        assertEquals("Deve retornar 0.0128205128 sal.",valorEsperado,valorRetornado,0);
-
-    }
+//    @Test
+//    public void somaTotalDeAlimentosDaCalculadoraTest(){
+//        Calculadora calculadora = new Calculadora();
+//
+//        Alimento alimento2 = new Alimento();
+//
+//        alimento2.setCodigo(1);
+//
+//        double valorEsperado = 0.0;
+//
+//        double valorRetornado = calculadora.getCaloriasTotal();
+//        assertEquals("Deve retornar zero calorias.", valorEsperado, valorRetornado, 0);
+//        valorRetornado = calculadora.getGorduraTotal();
+//        assertEquals("Deve retornar zero gordura.",valorEsperado,valorRetornado,0);
+//        valorRetornado = calculadora.getSodioTotal();
+//        assertEquals("Deve retornar zero sal.",valorEsperado,valorRetornado,0);
+//        valorRetornado = calculadora.getAcucarTotal();
+//        assertEquals("Deve retornar zero açucar.",valorEsperado,valorRetornado,0);
+//
+//        calculadora.adicionaAlimento(alimento2);
+//
+//        valorEsperado = 2000.0;
+//        valorRetornado = calculadora.getCaloriasTotal();
+//        assertEquals("Deve retornar 2000 calorias.",valorEsperado,valorRetornado,0);
+//
+//        valorEsperado = 2000.0;
+//        valorRetornado = calculadora.getGorduraTotal();
+//        assertEquals("Deve retornar 2000 gordura.",valorEsperado,valorRetornado,0);
+//
+//        valorEsperado = 5.0;
+//        valorRetornado = calculadora.getSodioTotal();
+//        assertEquals("Deve retornar 2000 sodio.",valorEsperado,valorRetornado,0);
+//
+//        valorEsperado = 2000.0;
+//        valorRetornado = calculadora.getAcucarTotal();
+//        assertEquals("Deve retornar 2000 acucar.",valorEsperado,valorRetornado,0);
+//
+//
+//        valorEsperado = 0.01;
+//        valorRetornado = calculadora.getSalTotal();
+//        assertEquals("Deve retornar 0.0128205128 sal.",valorEsperado,valorRetornado,0);
+//
+//    }
 }
