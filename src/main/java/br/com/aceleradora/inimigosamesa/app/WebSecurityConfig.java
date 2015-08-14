6dd02abf32af372f5be9f50e2e7ecc979345a8e5
lucassveloso <lucassveloso@hotmail.com>
2015-08-14 13:51:05 -0300

    #00 <Lucas, Amanda, Raphael, Paty> - Adicionando link de cadastroUsuario da configuração do spring para visualização apenas a admins

--- src/main/java/br/com/aceleradora/inimigosamesa/app/WebSecurityConfig.java
+++ src/main/java/br/com/aceleradora/inimigosamesa/app/WebSecurityConfig.java
@@ -24,7 +24,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/cadastroAlimento", "/editarAlimento/**")
                 .hasAnyAuthority("USER", "ADMIN")
-                .antMatchers("/formularioDeletarUsuario","/editarUsuario","/deletarUsuario").hasAnyAuthority("ADMIN")
+                .antMatchers("/formularioDeletarUsuario","/editarUsuario",
+                        "/cadastrarUsuario","/deletarUsuario").hasAnyAuthority("ADMIN")
                 .antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
