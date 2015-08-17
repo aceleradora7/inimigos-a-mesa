8383c658a2d20ab2f4903d26e57b8a7dcf0a646e
amandasv <amandasv.pm@gmail.com>
2015-08-17 15:41:20 -0300

    00 <Amanda, Lucas, Raphael> Criando js para modal Deletado com Sucesso

---
+++ src/main/resources/static/js/deletarAlimento.js
@@ -0,0 +1,13 @@
+//<![CDATA[
+
+$(document).ready(function(){
+
+var link = window.location.href;
+    if(link.indexOf("alimentoDeletado") > 0){
+        $('#alimento-deletado').modal('show');
+    }
+
+    });
+
+//]]>
+
